4ea17b43e1a864e33f19ca6b35a8115f23b9c1af
Paulina Kaczo <p-cioch@wp.pl>
2023-06-16 15:07:59 +0200

    add components directory and Header components in it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
+import Header from "./components/Header.jsx";
 
 function App() {
 
   return (
-    <>
-      <div>
-      </div>
-    </>
+    <div className="App">
+      <Header/>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+        <h1>To do</h1>
+    );
+}
+
+export default Header;
